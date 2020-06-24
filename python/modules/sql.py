d4428bdd4f8b4d88fcc3cff9b62ef08ed80a6e4f
blipp <bclipp770@gmail.com>
2020-06-23 22:11:20 -0400

    table creation

---
+++ python/modules/sql.py
@@ -0,0 +1,63 @@
+def create_song_plays() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS song_plays(
+    id  SERIAL PRIMARY KEY, 
+    start_time date REFERENCES time(start_time), 
+    user_id int NOT NULL REFERENCES users(user_id), 
+    level text, 
+    song_id text REFERENCES songs(song_id), 
+    artist_id text REFERENCES artists(artist_id), 
+    session_id int, 
+    location text, 
+    user_agent text);
+"""
+
+
+def create_users() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS users (
+    id  SERIAL PRIMARY KEY, 
+    user_id int , 
+    first_name text NOT NULL, 
+    last_name text NOT NULL, 
+    gender text, 
+    level text);
+"""
+
+
+def create_songs() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS songs (
+    id  SERIAL PRIMARY KEY, 
+    song_id text, 
+    title text NOT NULL, 
+    artist_id text NOT NULL REFERENCES artists(artist_id), 
+    year int, 
+    duration float NOT NULL);
+"""
+
+
+def create_artists() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS artists(
+    id  SERIAL PRIMARY KEY,
+    artist_id text,
+    name text NOT NULL, 
+    location text, 
+    lattitude float, 
+    longitude float);
+"""
+
+
+def create_time() -> str:
+    return """
+    CREATE TABLE IF NOT EXISTS time( 
+    id  SERIAL PRIMARY KEY,
+    start_time date,
+    hour int, 
+    day int, 
+    week int, 
+    month int, 
+    year int, 
+    weekday text);
+"""
