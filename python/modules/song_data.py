273575b32adf1218ecaaeea8d6ffc0255dc398b6
blipp <bclipp770@gmail.com>
2020-07-10 06:47:48 -0400

    fixing issue

--- python/modules/song_data.py
+++ python/modules/song_data.py
@@ -5,6 +5,8 @@ import modules.database as db
 def process_song_data(config: dict, file_path: str):
     data_frame = pd.read_json(file_path, lines=True)
     database_manager: db.DatabaseManager = db.DatabaseManager(config)
-    database_manager.df_insert(data_frame, "artists", "artist_id")
-    database_manager.df_insert(data_frame, "songs", "song_id")
+    artist = data_frame[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']]
+    database_manager.df_insert(artist, "artists", "artist_id")
+    song = data_frame[['song_id','title', 'artist_id', 'year', 'duration']]
+    database_manager.df_insert(song, "songs", "song_id")
     database_manager.close_conn()
